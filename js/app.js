c736c3e2b6da70bb9fdea9539e9985b9e7c48fcf
AJKAR <karczewskiamanda@gmail.com>
2022-02-07 23:06:11 -0600

    define variables

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
+let boardState = []
+let playerTurn
+let winner
 
 
 
@@ -16,20 +19,6 @@
 
 /*-------------------------------- Functions --------------------------------*/
 
-
-
-
-// 1) Define the required variables used to track the state of the game:
-  // None of these variables will need to hold a value when they are defined
-
-	// 1.1) Use an array to represent the squares on the board.    
-
-	// 1.2) Use a turn variable to track whose turn it is.
-
-	// 1.3) Use a winner variable to represent three different game states:
-	  // a player that won
-	  // a tie has occured
-	  // or a game that is still in play.
 
 
 // 2) Store cached element references on the page that will be accessed in code more than once in variables to make code more concise, readable, and performant:
